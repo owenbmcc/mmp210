ac418eb1f2c80f2c21317411bc335c47955e6997
owenbmcc <oroberts@bmcc.cuny.edu>
2020-02-04 17:35:24 -0500

    adds self portrait sketch

---
+++ self_portrait/sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+	createCanvas(640, 360);
+}
+
+function draw() {
+	background("lightgreen");
+
+	ellipse(200, 180, 200);
+	ellipse(220, 160, 20);
+	ellipse(160, 160, 30, 10);
+}
