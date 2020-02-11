208a12b7e49a2abab04a899270d8521ac136d417
owenbmcc <oroberts@bmcc.cuny.edu>
2020-02-11 17:02:37 -0500

    adds self portrait with variables

---
+++ variables/sketch.js
@@ -0,0 +1,70 @@
+/*
+	self potrait with variables
+	by owen 
+	2.11.2020
+	class example
+*/
+
+//  global scope
+
+// position variables
+var x = 200; // origin for face
+var y = 180;
+
+// size variables
+var s = 200; // size of the face
+
+function setup() {
+	// setup scope
+	createCanvas(640, 360);
+}
+
+function draw() {
+	// draw scope
+	background(100, 200, 100);
+
+	// shapes with xy coordinate + size 
+
+	fill('green');
+	stroke('white');
+	strokeWeight(5);	
+
+	// face area
+	ellipse(x, y, s);  // face
+	
+	// x++;
+	// s++;
+
+	fill(0, 81, 82, 127);
+	stroke(0, 81, 82);
+	ellipse(x + 20, y - 20, s/10); // right eye
+	ellipse(x - 40, y - 20, s/8, s/20); // left eye
+
+	// mouth
+	rectMode(CENTER);
+	stroke('#5DADE2');	
+	strokeWeight(2);
+	noFill();
+	rect(x, y + 40, 100, 20, 10);
+
+	// right ear
+	stroke('white');
+	noFill();
+	arc(x + 90, y, 50, 60, -HALF_PI, HALF_PI);
+	arc(x + 90, y, 40, 50, -HALF_PI, HALF_PI);
+
+	// shapes that are only xy points
+
+	// hair
+	line(x - 50, y - 110, x - 40, y - 80);
+	line(x - 40, y - 110, x - 30, y - 80);
+	line(x - 30, y - 110, x - 20, y - 80);
+	line(x - 20, y - 110, x - 10, y - 80);
+	line(x - 10, y - 110, x, y - 80);
+
+	// nose
+	fill('DARKTURQUOISE');
+	noStroke();
+	triangle(x - 10, y, x - 20, y + 20, x + 20, y + 10);
+
+}
