db4a561e5162455e73a4d23ba5b7fc7634d66ae2
owenbmcc <oroberts@bmcc.cuny.edu>
2020-09-07 18:43:07 -0400

    cleaned up self portrait

--- self_portrait_v1/sketch.js
+++ self_portrait_v1/sketch.js
@@ -8,34 +8,7 @@ function setup() {
 }
 
 function draw() {
-	background('green');
 
-	// face
-	noStroke();
-	fill(162, 108, 227); // purple
-	triangle(380, 50, 600, 60, 500, 360);
+	// draw self portrait here
 	
-	
-	// eyes
-	fill("LIGHTGOLDENRODYELLOW");
-	circle(430, 150, 100); // left
-	fill("#FFFF0B"); // similar to lightgoldenrodyellow
-	ellipse(550, 150, 100, 50); // right
-
-	// mouth
-	stroke("LIGHTGOLDENRODYELLOW");
-	strokeWeight(10);
-	noFill();
-	rect(450, 250, 100, 20, 10);
-
-	// hair
-	stroke(254, 212, 143); // yellowish brown
-	line(400, 40, 430, 80);
-	line(420, 40, 450, 80);
-	line(440, 40, 470, 80);
-	line(460, 40, 490, 80);
-	line(480, 40, 510, 80);
-	line(500, 40, 530, 80);
-	line(520, 40, 550, 80);
-
 }
